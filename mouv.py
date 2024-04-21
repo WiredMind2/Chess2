65a6f2c5a837e9b3aacfefe6e9c5f0abef74a51f
laborylouis <165661904+laborylouis@users.noreply.github.com>
2024-04-21 17:50:44 +0200

    mouvement pion

--- mouv.py
+++ mouv.py
@@ -55,7 +55,7 @@ class Movement:
 				mouvement_dispo.append(case_adjcante2[Dir.DOWN])
 			return mouvement_dispo
 		
-		elif self.board[y][x] != None and self.board[y][x].team == 'R':
+		if self.board[y][x] != None and self.board[y][x].team == 'R':
 			x,y = self.coords_to_index(case_adjcante[Dir.LEFT])
 			if self.board[y][x] == None and (x>-1 and x<12) and (y>-1 and y<12):
 				mouvement_dispo.append(case_adjcante[Dir.LEFT])
@@ -73,7 +73,7 @@ class Movement:
 				mouvement_dispo.append(case_adjcante2[Dir.DOWN])
 			return mouvement_dispo
 		
-		else:
+		if self.board[y][x] != None and self.board[y][x].team == 'B':
 			x,y = self.coords_to_index(case_adjcante[Dir.LEFT])
 			if self.board[y][x] == None and (x>-1 and x<12) and (y>-1 and y<12):
 				mouvement_dispo.append(case_adjcante[Dir.LEFT])
