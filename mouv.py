46b9bc5cd27478bfbfc8a0f8a99464017091c7cf
WiredMind2 <william@michaud.re>
2024-05-01 20:02:11 +0200

    Fixed list_moves

--- mouv.py
+++ mouv.py
@@ -18,7 +18,6 @@ class Movement:
 
 	@classmethod
 	def index_to_coords(cls, x, y):
-		
 		if y <= 3:
 			# White zone
 			pass  # Don't change anything
@@ -71,7 +70,11 @@ class Movement:
 		return out
 
 	def get_adjacent(self, coords):
-		x, y = self.coords_to_index(coords)
+		if isinstance(coords, str):
+			x, y = self.coords_to_index(coords)
+		else:
+			x, y = coords
+
 		for dir, (dx, dy) in [(Dir.DOWN, (-1, 0)), (Dir.UP, (1, 0)), (Dir.RIGHT, (0, -1)), (Dir.LEFT, (0, 1))]:
 			nx, ny = x+dx, y+dy
 
